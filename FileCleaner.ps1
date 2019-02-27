5dfd7d26f5a3e1c41aef49d312d376643c20f104
Benjamin Riley <benriley94@msn.com>
2019-02-27 17:08:11 +1100

    Initial commit

---
+++ FileCleaner.ps1
@@ -0,0 +1,43 @@
+﻿param (
+    [Parameter(Mandatory=$True, Position=1)]
+    [string[]]$Path,
+    [switch]$WhatIf
+)
+
+Set-StrictMode -Version Latest
+
+
+
+function Clean([string[]]$Path, [switch]$WhatIf)
+{
+    # Clear out files
+    $cutoff = (Get-Date) - (New-TimeSpan -Days 30)
+    $oldFiles = Get-ChildItem -Path $Path -Recurse -Force | Where-Object {-not $_.PSIsContainer -and $_.LastAccessTime -lt $cutoff}
+    foreach($item in $oldFiles)
+    {
+        Remove-Item $item.PSPath -Force -WhatIf:$WhatIf
+    }
+
+    # Clear empty directories
+    CleanEmptyDirectories -Path $Path -WhatIf:$WhatIf
+}
+
+function CleanEmptyDirectories([string[]]$Path, [switch]$WhatIf)
+{
+    $folders = Get-ChildItem -Path $Path -Force | Where-Object {$_.PSIsContainer}
+    foreach ($folder in $folders)
+    {
+        if ((Get-ChildItem -Path $folder.PSPath -Recurse -Force | Where-Object {-not $_.PSIsContainer} | Measure-Object).Count -eq 0)
+        {
+            Remove-Item $folder.PSPath -Recurse -WhatIf:$WhatIf
+        }
+        else
+        {
+            CleanEmptyDirectories($folder.PSPath)
+        }
+    }
+}
+
+
+
+Clean -Path $Path -WhatIf:$WhatIf
